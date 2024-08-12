530918ea72d5a8b7b9ceb756b0489dd507f372bb
jamie-arun <141943739+jamie-arun@users.noreply.github.com>
2024-08-12 18:11:24 +0530

    Update approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -5,31 +5,30 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-const fxRootContractABI = require("../../Mumbai-Polygon-Bridge-main/fxRootContractABI.json");
-const tokenContractJSON = require("../artifacts/contracts/YellowChikky.sol/YellowChikky.json");
+const fxRootContractABI = require("../fxRootContractABI.json");
+const tokenContractJSON = require("../artifacts/contracts/AnimalRacoon.sol/AnimalRacoon.json");
 
-const tokenAddress = "0x8a6aFFd0c03C2d7c14d02Ad496Ca9fe8Ec2b47BC"; // place your erc271a contract address here
+const tokenAddress = "0xfE977a4c9f6Ef20DbAD829a85912C16c0a7294D2"; // place your erc271a contract address here
 const tokenABI = tokenContractJSON.abi;
 const fxERC271ARootAddress = "0x9E688939Cb5d484e401933D850207D6750852053";
-const walletAddress = "0xc0a0c84d598dDE03E42922Ad4f02368BCca867d7"; // place your public address for your wallet here
+const walletAddress = "0x3A8096e60391B865Da1d9E11Dc22a8a59492C691"; // place your public address for your wallet here
 
 async function main() {
 
     const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
     const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC271ARootAddress);
     
-    const approveTx = await tokenContract.setApprovalForAll(fxERC271ARootAddress,true);
+    const approveTx = await tokenContract.setApprovalForAll(fxERC271ARootAddress, true);
     await approveTx.wait();
 
     console.log('Approval confirmed');
 for( i=0;i<5;i++){
-
     const depositTx = await fxContract.deposit(tokenAddress, walletAddress, i, "0x6556");
     await depositTx.wait();
 }
 
-    console.log("Your NFT's Has Been Transfered");
-    
+    console.log("NFT transfer successfull");
+    console.log("Your given Balance: " + await tokenContract.balanceOf(walletAddress) + " NFT'S");
   
   }
   
